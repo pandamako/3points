08c28e3a92083c75602fb96e53d996d17b187089
Artem Bykov <artem.bikov@gmail.com>
2019-01-21 13:56:45 +0300

    Updates

---
+++ precache-manifest.2961ebbc0333e0096787aa8aab263ae8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2edef8a27b07bfb103ce",
+    "url": "/3points/static/css/main.15d55731.chunk.css"
+  },
+  {
+    "revision": "2edef8a27b07bfb103ce",
+    "url": "/3points/static/js/main.2edef8a2.chunk.js"
+  },
+  {
+    "revision": "7ae9b01d8c9a8549216d",
+    "url": "/3points/static/js/1.7ae9b01d.chunk.js"
+  },
+  {
+    "revision": "6333789d462d828abeb8",
+    "url": "/3points/static/js/runtime~main.6333789d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/3points/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ef0baa6f963d88cea50321e93ad64988",
+    "url": "/3points/index.html"
+  }
+];
