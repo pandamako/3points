a1b4eea12813730f9289665f5bae660dc11d76e4
Artem Bykov <artem.bikov@gmail.com>
2020-07-29 14:19:21 +0300

    Updates

---
+++ precache-manifest.f447cb62f5c60f7e98d4ac370541a70a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a1de6398c7b13603b3b13e73ba8cee6",
+    "url": "/3points/index.html"
+  },
+  {
+    "revision": "9ec2546c074026bbce5e",
+    "url": "/3points/static/css/main.8afc3199.chunk.css"
+  },
+  {
+    "revision": "04eacddfcc7bb782c4b9",
+    "url": "/3points/static/js/2.3a1dc4e9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/3points/static/js/2.3a1dc4e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec2546c074026bbce5e",
+    "url": "/3points/static/js/main.b4a6886a.chunk.js"
+  },
+  {
+    "revision": "ff3e6a9e4cc693531afe",
+    "url": "/3points/static/js/runtime-main.d9e1074e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/3points/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/3points/precache-manifest.aab32f9f8af67e788545d700375fadfc.js"
+  "/3points/precache-manifest.f447cb62f5c60f7e98d4ac370541a70a.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/3points/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/3points/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
